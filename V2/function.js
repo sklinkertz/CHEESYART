24c9d3f93acf28983d3e755db892e9ae6a0affe9
Simon Klinkertz <simon.klinkertz@twt.de>
2016-10-22 16:59:22 +0200

    ADDED V1 V2

---
+++ V2/function.js
@@ -0,0 +1,43 @@
+(function () {
+    $(document).ready(function () {
+        var clientBgColor, count, facebookBgColor, funcHoverLinkAndChangeBodyBg, githubBgColor, mainBgColor, middleHeight, middleOffset, textArray, vimeoBgColor;
+        middleHeight = $('.middle').height();
+        middleOffset = middleHeight / 2;
+        $('.middle').css({ 'margin-top': '-' + middleOffset + 'px' });
+        mainBgColor = '#151D20';
+        facebookBgColor = '#3B5998';
+        githubBgColor = '#5F2B80';
+        vimeoBgColor = '#00B3EC';
+        clientBgColor = '#f1c40f';
+        count = 0;
+        textArray = [
+            'Jo!',
+            'MY NAME IS',
+            'SIMON "KLINKE" KLINKERTZ',
+            'AND I AM',
+            'FILMMAKER AND WEB-DESIGNER'
+        ];
+        funcHoverLinkAndChangeBodyBg = function (classLink, bgColor) {
+            $(classLink).mouseover(function () {
+                return $('body').css({ 'background': bgColor });
+            });
+            $(classLink).click(function () {
+                return $('body').css({ 'background': bgColor });
+            });
+            return $(classLink).mouseleave(function () {
+                return $('body').css({ 'background': mainBgColor });
+            });
+        };
+        funcHoverLinkAndChangeBodyBg('a.facebook', facebookBgColor);
+        funcHoverLinkAndChangeBodyBg('a.github', githubBgColor);
+        funcHoverLinkAndChangeBodyBg('a.vimeo', vimeoBgColor);
+        funcHoverLinkAndChangeBodyBg('a.client', clientBgColor);
+        return $('.clickme').click(function () {
+            $('.whoAmI').text(textArray[count]);
+            count++;
+            if (count === textArray.length) {
+                return count = 0;
+            }
+        });
+    });
+}.call(this));
